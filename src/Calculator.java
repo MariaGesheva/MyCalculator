432debfb925c3cb1ac524d9c32d79f231114aeda
Maria Gesheva (geshevam) <geshevam@coventry.ac.uk>
2022-06-14 11:39:00 +0100

    Initial commit Demo connection

---
+++ src/Calculator.java
@@ -0,0 +1,14 @@
+public class Calculator {
+    public static int add(int num1, int num2) {
+        return num1 + num2;
+    }
+        public static void main(String[] args){
+            int total = add(1, 2);
+            System.out.println(total);
+        }
+
+    public int division(int num1, int num2) {
+        return  num1 / num2;
+    }
+}
+
